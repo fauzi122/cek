c75618700329e6de4c344b042c5fdbcc5841e3c3
fauzi ahmad <ozanfauzi39@gmail.com>
2024-05-29 20:38:30 +0700

    bap

--- resources/views/admin/ujian/uts/adm/rekap_bap/index.blade.php
+++ resources/views/admin/ujian/uts/adm/rekap_bap/index.blade.php
@@ -19,22 +19,12 @@
 							
 								<h4 class="m-b-0 text-white">
 									
-									REKAPITULASI BAP ALL KAMPUS</h4>
+									REKAPITULASI BAP </h4>
 							</div>
-							@if (session('success'))
-							<div class="alert alert-info">
-								{{ session('success') }}
-							</div>
-							@endif
-			
-							@if (session('error'))
-							<div class="alert alert-info">
-								{{ session('error') }}
-							</div>
-							@endif
+							
                             
 							<div class="table-container">
-                                <form action="/cari/rekap-bap" method="GET">
+								<form action="/cari/rekap-bap" method="GET">
 									<table class="table custom-table">
 										<tr>
 											<td>KD Dosen</td>
@@ -42,29 +32,32 @@
 										</tr>
 										<tr>
 											<td>KEL-UJIAN</td>
-											<td>
-												<input type="text" name="kel_ujian" placeholder="Masukkan Kelompok Ujian Mahasiswa" class="nilai form-control">
-											</td>
+											<td><input type="text" name="kel_ujian" placeholder="Masukkan Kelompok Ujian Mahasiswa" class="nilai form-control"></td>
 										</tr>
 										<tr>
 											<td>Kampus</td>
-                                        
-                                                <td>
-                                                    <select class="nilai form-control select2" name="no_ruang">
-                                                        <option value="">--Pilih Kampus--</option> <!-- Opsi ini harus di luar loop -->
-                                                        @foreach ($kampus as $kampus)
-                                                            <option value="{{ $kampus->kd_kampus }}">{{ $kampus->nm_kampus }} - {{ $kampus->kd_kampus }}</option>
-                                                        @endforeach
-                                                    </select>
-                                                </td>
-                                         
-                                  
-										</tr>
-                                        <tr>
-											<td>Tanggal Ujian</td>
 											<td>
-												<input type="date" name="tgl_ujian" placeholder="Masukkan tgl ujian" class="nilai form-control">
+												<select class="nilai form-control select2" name="no_ruang">
+													<option value="">--Pilih Kampus Semua Kampus--</option>
+													@foreach ($kampus as $k)
+														<option value="{{ $k->kd_kampus }}">{{ $k->nm_kampus }} - {{ $k->kd_kampus }}</option>
+													@endforeach
+												</select>
 											</td>
+										</tr>
+										<tr>
+											<td>Paket</td>
+											<td>
+												<select class="nilai form-control" name="paket" required>
+													<option value="">--Pilih Paket--</option>
+													<option value="UTS">UTS</option>
+													<option value="UAS">UAS</option>
+												</select>
+											</td>
+										</tr>
+										<tr>
+											<td>Tanggal Ujian</td>
+											<td><input type="date" name="tgl_ujian" class="nilai form-control"></td>
 										</tr>
 										<tr>
 											<td colspan="2" style="text-align: right;">
@@ -74,64 +67,8 @@
 										</tr>
 									</table>
 								</form>
-								<div class="table-responsive">
-									<table id="copy-print-csv" class="table custom-table">
-										<thead>
-											<tr>
-											  
-                                                <th>no</th>
-                                                <th>dosen</th>
-                                                <th>mtk</th>
-                                                <th>kel-ujian</th>
-                                                <th>ruang</th>
-                                                <th>paket</th>
-                                                <th>hari</th>
-                                                <th>tgl_ujian</th>
-                                                <th>isi bap</th>
-                                                <th>sts</th>
-                                                <th>ot</th>
-                                                <th>kampus</th>
-
-                                               
-											</tr>
-										</thead>
-										<tbody>
-											 @foreach ($result as $no => $jadwal)
-											<tr>
-											
-										
-											 <td>{{$loop->iteration}}</td>
-											 <td>{{ $jadwal->kd_dosen }}</td>
-                                             <td>{{ $jadwal->kd_mtk }}</td>
-											 <td>{{ $jadwal->kel_ujian }}</td>
-											 <td>{{ $jadwal->no_ruang }}</td>
-											 <td>{{ $jadwal->paket }}</td>
-											 <td>{{ $jadwal->hari }}</td>
-											 <td>{{ $jadwal->tgl_ujian }}</td>
-											 <td>{{ $jadwal->isi }}</td>
-											 <td>
-                                            </button>
-											@if ($jadwal->verifikasi == 1)
-											<span class='badge badge-pill badge-light'><h6>Ujian Lancar</h6></span>
-										@elseif ($jadwal->verifikasi == 2)
-											<span class='badge badge-pill badge-secondary'><h6>Ujian Bermasalah</h6></span>
-										@else
-                                        @endif
-                                            </td>
-											 <td>{{ $jadwal->ot }}</td>
-											 <td>{{ $jadwal->nm_kampus }}</td>
-											
-											
-
-											</tr>
-											@endforeach 
-										</tbody>
-						    	</table>
 								</div>
 							</div>
-
-						
-
 
 						</div>
 
